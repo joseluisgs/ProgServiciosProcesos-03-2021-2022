6944a2a35e03b6bc77390d9643f5c9880af08d14
MarioDam <marioturan.mv@gmail.com>
2021-12-09 18:21:01 +0100

    s

--- 09-ChatObservable/src/main/java/es/joseluisgs/dam/cliente/Cliente.java
+++ 09-ChatObservable/src/main/java/es/joseluisgs/dam/cliente/Cliente.java
@@ -20,21 +20,20 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 /**
- *
  * @author link
  */
 public class Cliente extends JFrame {
-    
+
     private JTextArea mensajesChat;
     private Socket socket;
-    
+
     private int puerto;
     private String host;
     private String usuario;
-    
-    public Cliente(){
+
+    public Cliente() {
         super("Cliente Chat");
-        
+
         // Elementos de la ventana
         mensajesChat = new JTextArea();
         mensajesChat.setEnabled(false); // El area de mensajes del chat no se debe de poder editar
@@ -43,16 +42,16 @@ public class Cliente extends JFrame {
         JScrollPane scrollMensajesChat = new JScrollPane(mensajesChat);
         JTextField tfMensaje = new JTextField("");
         JButton btEnviar = new JButton("Enviar");
-        
-        
+
+
         // Colocacion de los componentes en la ventana, lo hago a pelo sin usar NetBeans
         Container c = this.getContentPane();
         c.setLayout(new GridBagLayout());
-        
+
         GridBagConstraints gbc = new GridBagConstraints();
-        
+
         gbc.insets = new Insets(20, 20, 20, 20);
-        
+
         gbc.gridx = 0;
         gbc.gridy = 0;
         gbc.gridwidth = 2;
@@ -61,37 +60,36 @@ public class Cliente extends JFrame {
         gbc.fill = GridBagConstraints.BOTH;
         c.add(scrollMensajesChat, gbc);
         // Restaura valores por defecto
-        gbc.gridwidth = 1;        
+        gbc.gridwidth = 1;
         gbc.weighty = 0;
-        
-        gbc.fill = GridBagConstraints.HORIZONTAL;        
+
+        gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.insets = new Insets(0, 20, 20, 20);
-        
+
         gbc.gridx = 0;
         gbc.gridy = 1;
         c.add(tfMensaje, gbc);
         // Restaura valores por defecto
         gbc.weightx = 0;
-        
+
         gbc.gridx = 1;
         gbc.gridy = 1;
         c.add(btEnviar, gbc);
-        
-           
-        
+
+
         // Ventana de configuracion inicial
         VentanaConfiguracion vc = new VentanaConfiguracion(this);
         host = vc.getHost();
         puerto = vc.getPuerto();
         usuario = vc.getUsuario();
-         
+
         this.setBounds(400, 100, 400, 500);
-        this.setTitle("Cliente chat: "+ this.usuario);
+        this.setTitle("Cliente chat: " + this.usuario);
         this.setVisible(true);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
-        
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
         System.out.println("Quieres conectarte a " + host + " en el puerto " + puerto + " con el nombre de ususario: " + usuario + ".");
-        
+
         // Se crea el socket para conectar con el Sevidor del Chat
         try {
             socket = new Socket(host, puerto);
@@ -100,16 +98,16 @@ public class Cliente extends JFrame {
         } catch (IOException ex) {
             System.err.println("No se ha podido conectar con el servidor (" + ex.getMessage() + ").");
         }
-        
+
         // Accion para el boton enviar
         btEnviar.addActionListener(new AccionEnviar(socket, tfMensaje, usuario));
-        
+
     }
-    
+
     /**
      * Recibe los mensajes del chat reenviados por el servidor
      */
-    public void recibirMensajesServidor(){
+    public void recibirMensajesServidor() {
         // Obtiene el flujo de entrada del socket
         DataInputStream entradaDatos = null;
         String mensaje;
@@ -120,12 +118,12 @@ public class Cliente extends JFrame {
         } catch (NullPointerException ex) {
             System.err.println("El socket no se creo correctamente. ");
         }
-        
-        GestionMensajes gm = new GestionMensajes( entradaDatos, mensajesChat);
+
+        GestionMensajes gm = new GestionMensajes(entradaDatos, mensajesChat);
         gm.start();
-        
+
     }
-    
+
     /**
      * @param args the command line arguments
      */
